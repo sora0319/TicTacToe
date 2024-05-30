a350f62ff840018c7f1354d81e06b10fcef2f665
tiktokyi <tiktokyi@gmail.com>
2024-05-31 00:31:02 +0900

    Feat : O,X 표시, 승리 조건 체크

--- src/Board.java
+++ src/Board.java
@@ -1,12 +1,14 @@
 import java.util.Arrays;
 
 public class Board {
-    private int w = 3;
-    private int h = 3;
-    private String[][] board = new String[w][h];
+    private final int w = 3;
+    private final int h = 3;
+    private boolean flag = true;
+    private final String[][] board = new String[w][h];
 
     public Board() {
         initBoard();
+        showBoard();
     }
 
     void initBoard() {
@@ -22,4 +24,23 @@ public class Board {
             System.out.println(Arrays.toString(board[i]));
         }
     }
+
+    void draw_OX(int i, int j) {
+        this.board[i][j] = this.flag ? "O" : "X";
+        this.flag = !flag;
+    }
+
+    boolean isWin() {
+        if (board[0][0].equals(board[0][1]) && board[0][1].equals(board[0][2])) return true;
+        if (board[1][0].equals(board[1][1]) && board[1][1].equals(board[1][2])) return true;
+        if (board[2][0].equals(board[2][1]) && board[2][1].equals(board[2][2])) return true;
+        if (board[0][0].equals(board[1][0]) && board[1][0].equals(board[1][2])) return true;
+        if (board[0][1].equals(board[1][1]) && board[1][1].equals(board[2][1])) return true;
+        if (board[0][2].equals(board[1][2]) && board[1][2].equals(board[2][2])) return true;
+        if (board[0][0].equals(board[1][1]) && board[1][1].equals(board[2][2])) return true;
+        if (board[0][2].equals(board[1][1]) && board[1][1].equals(board[2][0])) return true;
+
+        return false;
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         Board board = new Board();
+        board.draw_OX(1,1);
+        System.out.println();
+        board.draw_OX(2,1);
+        System.out.println();
+        board.draw_OX(0,0);
+        System.out.println();
+        board.draw_OX(2,2);
+        System.out.println();
+        board.draw_OX(2,0);
+        System.out.println();
+        board.draw_OX(1,0);
+        System.out.println();
+        board.draw_OX(0,1);
+        System.out.println();
         board.showBoard();
+
     }
 }
