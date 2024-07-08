7f215731159ddfa251fe8d3c3b6b230e88697158
sora0319 <sojeoung536985@gmail.com>
2024-07-09 04:20:54 +0900

    Fix : fix the global hook control

--- src/module/snakegame/KeyListener.java
+++ src/module/snakegame/KeyListener.java
@@ -5,8 +5,14 @@ import com.github.kwhat.jnativehook.NativeHookException;
 import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent;
 import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
 
-public class KeyListener implements NativeKeyListener {
-    public KeyListener() {
+import java.util.Observable;
+
+public class KeyListener  extends Observable implements NativeKeyListener {
+    private static int lastKeyCode;
+    private static KeyListener instance;
+
+    // private 생성자로 외부에서의 인스턴스 생성 방지
+    private KeyListener() {
         try {
             // JNativeHook 라이브러리 초기화 및 등록
             GlobalScreen.registerNativeHook();
@@ -15,38 +21,35 @@ public class KeyListener implements NativeKeyListener {
             System.err.println(ex.getMessage());
             System.exit(1);
         }
-
-        // 이벤트 리스너 추가
-        GlobalScreen.addNativeKeyListener(this);
     }
+
+    // 싱글톤 인스턴스를 반환하는 메서드
+    public static synchronized KeyListener getInstance() {
+        if (instance == null) {
+            instance = new KeyListener();
+        }
+        return instance;
+    }
+
+//    public KeyListener() {
+//        try {
+//            // JNativeHook 라이브러리 초기화 및 등록
+//            GlobalScreen.registerNativeHook();
+//        } catch (NativeHookException ex) {
+//            System.err.println("There was a problem registering the native hook.");
+//            System.err.println(ex.getMessage());
+//            System.exit(1);
+//        }
+//
+//        // 이벤트 리스너 등록
+//        GlobalScreen.addNativeKeyListener(this);
+//    }
 
     @Override
     public void nativeKeyPressed(NativeKeyEvent e) {
-        switch (e.getKeyCode()) {
-            case NativeKeyEvent.VC_UP:
-                System.out.println("Up arrow key pressed");
-                break;
-            case NativeKeyEvent.VC_DOWN:
-                System.out.println("Down arrow key pressed");
-                break;
-            case NativeKeyEvent.VC_LEFT:
-                System.out.println("Left arrow key pressed");
-                break;
-            case NativeKeyEvent.VC_RIGHT:
-                System.out.println("Right arrow key pressed");
-                break;
-            case NativeKeyEvent.VC_ESCAPE:
-                System.out.println("ESC key pressed. Exiting...");
-                try {
-                    GlobalScreen.unregisterNativeHook();
-                } catch (NativeHookException ex) {
-                    ex.printStackTrace();
-                }
-                System.exit(0);
-                break;
-            default:
-                break;
-        }
+        lastKeyCode = e.getKeyCode();
+        setChanged();
+        notifyObservers(lastKeyCode);
     }
 
     @Override
@@ -59,7 +62,16 @@ public class KeyListener implements NativeKeyListener {
         // Do nothing
     }
 
-    public void stop() {
+    public void registerHook(){
+        // 이벤트 리스너 등록
+        GlobalScreen.addNativeKeyListener(this);
+    }
+
+    public void removeHook() {
+        GlobalScreen.removeNativeKeyListener(this);
+    }
+
+    public void unregisterHook() {
         try {
             GlobalScreen.unregisterNativeHook();
         } catch (NativeHookException e) {
