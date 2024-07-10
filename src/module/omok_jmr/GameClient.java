dbcff51c898d6e52c980e2d2df1d47b386eacbc3
Miru <alfn051@gmail.com>
2024-07-10 09:33:41 +0900

    Feat : 오목 바둑판 및 돌 관련 기본 기능 추가

--- src/module/omok_jmr/GameClient.java
+++ src/module/omok_jmr/GameClient.java
@@ -1,9 +1,24 @@
 package module.omok_jmr;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameClient{
 
-    public void run() {
+    Board board = new Board();
+    UI ui = new UI();
+    Color first;    //선공 색깔
+    List<Stone> history = new ArrayList<>();    //기보
 
+    public void run() {
+        first = ui.startMessage();
+        board.putStone(new Stone(Color.BLACK, 5, 5));
+        ui.showBoard(board);
+        while (true) {
+            ui.showBoard(board);
+
+        }
     }
+
 
 }
--- src/module/omok_jmr/Stone.java
+++ src/module/omok_jmr/Stone.java
@@ -1,10 +1,31 @@
 package module.omok_jmr;
 
 public class Stone {
-    Color color;
+    private Color color;
+    private int x;
+    private int y;
 
+    public Stone(Color color, int x, int y) {
+        this.color = color;
+        this.x = x;
+        this.y = y;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public String toString() {
+        return color.toString();
+    }
 }
 
-enum Color{
-    WHITE, BLACK
-}
+
